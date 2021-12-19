e9797463e72e27aa455526f99a28d13be02b56dd
Vicente Velasco <velasco.josevicente@gmail.com>
2021-12-18 23:49:44 -0500

    Se añaden populate a preguntas

--- modelos/pregunta/resolversPregunta.js
+++ modelos/pregunta/resolversPregunta.js
@@ -4,16 +4,16 @@ const resolversPregunta={
 
     Query:{
         readPreguntas : async (parent, args)=>{
-            const preguntas = await PreguntaModel.find({ronda:args.ronda}).populate('respuestas')
+            const preguntas = await PreguntaModel.find({ronda:args.ronda}).populate('respuestas').populate('premio')
             return preguntas
         },
         readPregunta: async (parent, args) => {
             if(Object.keys(args).includes('_id')){
-                const pregunta =await PreguntaModel.findOne({ _id:args.id}).populate('respuestas')
+                const pregunta =await PreguntaModel.findOne({ _id:args.id}).populate('respuestas').populate('premio')
                 return pregunta
             }
             else if (Object.keys(args).includes('enunciado')){
-                const pregunta =await PreguntaModel.findOne({ enunciado:args.enunciado}).populate('respuestas')
+                const pregunta =await PreguntaModel.findOne({ enunciado:args.enunciado}).populate('respuestas').populate('premio')
                 return pregunta
             }
         },
@@ -25,7 +25,7 @@ const resolversPregunta={
                 enunciado:args.enunciado,
                 premio: args.premio,
                 ronda: args.ronda,
-            })
+            }).populate('respuestas').populate('premio')
             return nuevaPregunta
         },
     },
